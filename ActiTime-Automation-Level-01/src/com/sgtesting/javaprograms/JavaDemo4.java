b572a41f185360216b09e902807c6796ae415b9e
shravaniC <cshravani.ise@gmail.com>
2022-01-13 15:38:05 +0530

    Delete ActiTime-Automation-Level-01 directory

--- ActiTime-Automation-Level-01/src/com/sgtesting/javaprograms/JavaDemo4.java
+++
@@ -1,9 +0,0 @@
-package com.sgtesting.javaprograms;
-
-public class JavaDemo4 {
-
-	public static void main(String[] args) {
-		System.out.println("Be a good person but dont waste time to prove it");
-	}
-
-}
--- ActiTime-Automation-Level-01/src/com/sgtesting/javaprograms/JavaDemo6.java
+++
@@ -1,7 +0,0 @@
-package com.sgtesting.javaprograms;
-
-public class JavaDemo6 {
-	public static void main(String[] args) {
-		System.out.println("Be a good person but dont waste time to prove it");
-	}
-}
